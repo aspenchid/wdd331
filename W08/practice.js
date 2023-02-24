b98b04612ab025a80e3c99c2ee43b3036ff4bac8
Aspen Chidester <chi21051@byui.edu>
2023-02-24 16:48:05 -0700

    nav w08

--- W08/practice.js
+++ W08/practice.js
@@ -1,6 +1,12 @@
-$(function() {
-    $('#hamburger').on('click', function() {
-      $(this).toggleClass('close');
-      $('#nav').toggleClass('visible');
-    });
-});
+
+function toggleHamburger() {
+  var updateElement = document.getElementById("hamburger");
+  var updateElement2 = document.getElementById("nav");
+
+//toggle adds a class if it's not there or removes it if it is.
+  updateElement.classList.toggle("close");
+  updateElement2.classList.toggle("visible");
+
+}
+
+
